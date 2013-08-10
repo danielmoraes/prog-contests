4c4a1d7439187aca75b63642f60112c64b07c70d
Daniel Moraes <daniel.b.moraes@gmail.com>
2013-08-10 12:25:27 -0300

    Improved answer for Adding Reversed Numbers (SPOJ).

--- spoj/42.addrev/ans.py
+++ spoj/42.addrev/ans.py
@@ -3,15 +3,7 @@
 # Author: Daniel Moraes (daniel.b.moraes@gmail.com)
 
 
-def revert(n):
-    n = list(n)
-    s = len(n)
-    for i in xrange(s / 2):
-        inv = s - i - 1
-        n[i], n[inv] = n[inv], n[i]
-    return int(''.join(n))
-
 t = int(raw_input())
 for c in xrange(t):
     n1, n2 = map(str, raw_input().split())
-    print '%d' % revert(str(revert(n1) + revert(n2)))
+    print int(str(int(n1[::-1]) + int(n2[::-1]))[::-1])
