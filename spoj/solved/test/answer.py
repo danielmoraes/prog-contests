ce9a622bbd35ed7eafa02cf5b74f5733a23be4d1
Daniel Moraes <daniel.b.moraes@gmail.com>
2013-08-25 21:07:39 -0300

    Adding solution for test (spoj).

---
+++ spoj/solved/test/answer.py
@@ -0,0 +1,8 @@
+# SPOJ 1
+# Life, the Universe, and Everything (TEST)
+# Author: Daniel Moraes (daniel.b.moraes@gmail.com)
+
+n = int(raw_input())
+while n != 42:
+    print n
+    n = int(raw_input())
