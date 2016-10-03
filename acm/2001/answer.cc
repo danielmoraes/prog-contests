0dfeb8c2c2c8eea4f66de884c05ecc6f83642a7d
Daniel Moraes <daniel.b.moraes@gmail.com>
2016-10-02 21:14:47 -0300

    Remove acm problems

--- acm/2001/answer.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(void) {
-    int a1, a2, a3, b1, b2, b3;
-    cin >> a1 >> b1 >> a2 >> b2 >> a3 >> b3;
-    cout << a1 - a3 << " " << b1 - b2 << endl;
-}
