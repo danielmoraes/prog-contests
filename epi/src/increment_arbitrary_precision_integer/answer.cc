749e7d51aaf208cf1b7e3bdcafe27d5301b235ba
Daniel Moraes <daniel.b.moraes@gmail.com>
2017-02-22 10:47:22 -0300

    Added solution to problem 6.2 of EPI.

---
+++ epi/src/increment_arbitrary_precision_integer/answer.cc
@@ -0,0 +1,43 @@
+/*
+ *
+ * EPI
+ * Increment an Arbitrary Precision Integer (6.2)
+ *
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print_vector(const vector<int>& v) {
+  for (int i = 0; i < v.size(); i++) {
+    if (i > 0) {
+      cout << " ";
+    }
+    cout << v[i];
+  }
+  cout << endl;
+}
+
+void increment(vector<int>& v) {
+  v.back()++;
+  for (int i = v.size() - 1; i > 0 && v[i] == 10; i--) {
+    v[i - 1]++;
+    v[i] = 0;
+  }
+  if (v[0] == 10) {
+    v[0] = 0;
+    v.insert(v.begin(), 1);
+  }
+}
+
+int main() {
+  vector<int> v1 = {1, 2, 9};
+  vector<int> v2 = {9, 9, 9};
+  increment(v1);
+  print_vector(v1);
+  increment(v2);
+  print_vector(v2);
+  return 0;
+}
